7bfa071279e25f24ed0530ae5cd0b94285fcb6fd
sanket kachole <sanketkachole0707@gmail.com>
2024-01-09 20:57:28 +0000

    Foundational Programming for NN in Pytorch

--- Foundational_Pytorch/Neural_Network.py
+++ Foundational_Pytorch/Neural_Network.py
@@ -27,3 +27,35 @@ num_data = 1000
 num_input = 1000
 num_hidden_layer_nodes = 100
 num_output = 10
+
+num_epochs = 100
+
+#Create input and output tensor
+
+x = torch.rand(num_data, num_input)
+y = torch.rand(num_data, num_output)
+
+# construct model
+model = Diffusion_model(num_input, num_hidden_layer_nodes, num_output)
+
+# Define Loss Function
+loss_function = nn.MSELoss(reduction = 'sum')
+
+# Define optimiser
+optimiser = optim.SGD(model.parameters(), lr=1e-4)
+
+
+for t in range(num_epochs):
+    y_pred = model(x)
+
+    loss = loss_function(y_pred, y)
+    print(t , loss)
+
+    optimiser.zero_grad()
+
+    loss.backward()
+
+    optimiser.step()
+
+
+
