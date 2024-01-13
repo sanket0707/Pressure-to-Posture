7d89ad3515de5b76c57829d23d3b85e99f5cd1c7
sanket kachole <sanketkachole0707@gmail.com>
2024-01-13 10:52:41 +0000

    Building the LeNet Architecture from scratch - Training Module

--- Foundational_Pytorch/LeNet.py
+++ Foundational_Pytorch/LeNet.py
@@ -1,98 +1,137 @@
+##%matplotlib inline
+import os
+import time
+
+from typing import Iterable, Tuple
+from dataclasses import dataclass
+
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt  # one of the best graphics library for python
 
 
 ################################################################
 #################### Step 1 - Datset ###########################
 ################################################################
-from torchvision import datasets, transforms
 
-def get_data(batch_size, data_root='root', num_worker=1):
-
-    train_test_traonsform = transforms.Compose([
-        transforms.Resize(size= ),
+def get_data(batch_size, data_root='data', num_workers=1):
+    train_test_transforms = transforms.Compose([
+        # Resize to 32X32
+        transforms.Resize((32, 32)),
+        # this re-scales image tensor values between 0-1. image_tensor /= 255
         transforms.ToTensor(),
-        transforms.Normalize((0.1307, ), (0.3081, ))
+        # subtract mean (0.1307) and divide by variance (0.3081).
+        # This mean and variance is calculated on training data (verify yourself)
+        transforms.Normalize((0.1307,), (0.3081,))
     ])
 
-
-    train_dataloader = torch.utils.data.Dataloader(
-        datasets.MNIST(root= , train= , download= False, transform= ),
-        batch_size = batch_size,
-        shuffle = True,
-        num_worker = num_worker
+    # train dataloader
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(root=data_root, train=True, download=True, transform=train_test_transforms),
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers
     )
 
-
-    test_dataloader = torch.utils.data.Dataloader(
-        datasets.MNIST(root= , test = , download= False, transform= ),
-        batch_size = batch_size,
-        shuffle = True,
-        num_worker = num_worker
+    # test dataloader
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(root=data_root, train=False, download=True, transform=train_test_transforms),
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers
     )
-
-    return train_dataloader, test_dataloader
-
+    return train_loader, test_loader
 
 
 ################################################################
 ################## Step 2 - LeNet Architecture##################
 ################################################################
-class LeNet(torch.nn.Module):
+class LeNet5(nn.Module):
     def __init__(self):
         super().__init__()
 
+        # convolution layers
         self._body = nn.Sequential(
-            nn.Conv2d(in_channels=1 , out_channels=6 , kernel_size= 5),
+            # First convolution Layer
+            # input size = (32, 32), output size = (28, 28)
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5),
+            # ReLU activation
             nn.ReLU(inplace=True),
+            # Max pool 2-d
             nn.MaxPool2d(kernel_size=2),
 
+            # Second convolution layer
+            # input size = (14, 14), output size = (10, 10)
             nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2)
+            nn.MaxPool2d(kernel_size=2),
+            # output size = (5, 5)
         )
 
+        # Fully connected layers
         self._head = nn.Sequential(
-            nn.Linear(in_features= 16*5*5 , out_features= 120),
+            # First fully connected layer
+            # in_features = total number of weights in last conv layer = 16 * 5 * 5
+            nn.Linear(in_features=16 * 5 * 5, out_features=120),
+
+            # ReLU activation
             nn.ReLU(inplace=True),
-            nn.Linear(in_features= 120 , out_features= 84),
+
+            # second fully connected layer
+            # in_features = output of last linear layer = 120
+            nn.Linear(in_features=120, out_features=84),
+
+            # ReLU activation
             nn.ReLU(inplace=True),
-            nn.Linear(in_features=84, out_features= 10)
+
+            # Third fully connected layer. It is also output layer
+            # in_features = output of last linear layer = 84
+            # and out_features = number of classes = 10 (MNIST data 0-9)
+            nn.Linear(in_features=84, out_features=10)
         )
 
-
-    def forward(self,x):
-        # Pass the image into main body ie. convolution
+    def forward(self, x):
+        # apply feature extractor
         x = self._body(x)
-        # Get the output of the main body and the flatten it.
-        x = x.view(x.size()[0],-1)
-        # pass that flattened output into head
+        # flatten the output of conv layers
+        # dimension should be batch_size * number_of weights_in_last conv_layer
+        x = x.view(x.size()[0], -1)
+        # apply classification head
         x = self._head(x)
-        # return the output
         return x
 
-LeNet_model = LeNet()
-print(LeNet_model)
+lenet5_model = LeNet5()
+print(lenet5_model)
 
 
 ################################################################
 ############ Step 3 - System, training Configurations ##########
 ################################################################
-@dataclasses
-class systemConfiguration:
+
+@dataclass
+class SystemConfiguration:
     '''
-        Describes the common system setting needed for reproducible training
-        '''
+    Describes the common system setting needed for reproducible training
+    '''
     seed: int = 42  # seed number to set the state of all random number generators
     cudnn_benchmark_enabled: bool = True  # enable CuDNN benchmark for the sake of performance
     cudnn_deterministic: bool = True  # make cudnn deterministic (reproducible training)
 
-@dataclasses
+
+
+
+
+
+@dataclass
 class TrainingConfiguration:
     '''
-        Describes configuration of the training process
-        '''
+    Describes configuration of the training process
+    '''
     batch_size: int = 32  # amount of data to pass through the network at each forward-backward iteration
     epochs_count: int = 20  # number of times the whole dataset will be passed through the network
     learning_rate: float = 0.01  # determines the speed of network's weights update
@@ -103,6 +142,10 @@ class TrainingConfiguration:
     device: str = 'cuda'  # device to use for training.
 
 
+
+
+
+
 def setup_system(system_config: SystemConfiguration) -> None:
     torch.manual_seed(system_config.seed)
     if torch.cuda.is_available():
@@ -110,47 +153,67 @@ def setup_system(system_config: SystemConfiguration) -> None:
         torch.backends.cudnn.deterministic = system_config.cudnn_deterministic
 
 
+
+
+
+
 ################################################################
 ################ Step 4 - Training #######################
 ################################################################
-import torch.nn.functional as F
-def train(train_config: TrainingConfiguration,
-          model: nn.Module,
-          optimizer: torch.optim.optimizer ,
-          train_loader: torch.utils.data.Dataloader,
-          epoch_idx: int ) -> tuple[float, float]:
+def train(
+        train_config: TrainingConfiguration,
+        model: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        train_loader: torch.utils.data.DataLoader,
+        epoch_idx: int
+) -> Tuple[float, float]:
     # change model in training mode
     model.train()
+
     # to get batch loss
     batch_loss = np.array([])
+
     # to get batch accuracy
     batch_acc = np.array([])
+
     for batch_idx, (data, target) in enumerate(train_loader):
+
         # clone target
         indx_target = target.clone()
         # send data to device (it is mandatory if GPU has to be used)
         data = data.to(train_config.device)
         # send target to device
         target = target.to(train_config.device)
+
         # reset parameters gradient to zero
         optimizer.zero_grad()
+
         # forward pass to the model
         output = model(data)
+
         # cross entropy loss
-        loss = F.cross_entropy(output,target)
+        loss = F.cross_entropy(output, target)
+
         # find gradients w.r.t training parameters
         loss.backward()
         # Update parameters using gradients
         optimizer.step()
+
+        batch_loss = np.append(batch_loss, [loss.item()])
+
         # get probability score using softmax
-        prob = F.softmax(output,dim=1)
+        prob = F.softmax(output, dim=1)
+
         # get the index of the max probability
         pred = prob.data.max(dim=1)[1]
+
         # correct prediction
         correct = pred.cpu().eq(indx_target).sum()
+
         # accuracy
         acc = float(correct) / float(len(data))
-        batch_acc = np.append(batch_acc,[acc])
+
+        batch_acc = np.append(batch_acc, [acc])
 
         if batch_idx % train_config.log_interval == 0 and batch_idx > 0:
             print(
@@ -158,8 +221,6 @@ def train(train_config: TrainingConfiguration,
                     epoch_idx, batch_idx * len(data), len(train_loader.dataset), loss.item(), acc
                 )
             )
-
-
 
     epoch_loss = batch_loss.mean()
     epoch_acc = batch_acc.mean()
@@ -169,44 +230,55 @@ def train(train_config: TrainingConfiguration,
 
 
 
+
+
+
+
 ################################################################
 ################ Step 5 - Validation  ######################
 ################################################################
-def val(train_config : TrainingConfiguration,
-        model : nn.Module,
-        test_loader : torch.utils.data.Dataloader
-) -> tuple[float, float] :
-
+def validate(
+        train_config: TrainingConfiguration,
+        model: nn.Module,
+        test_loader: torch.utils.data.DataLoader,
+) -> Tuple[float, float]:
     model.eval()
     test_loss = 0
-    count_correct_prediction = 0
+    count_corect_predictions = 0
 
-    with torch.not_equal():
-
+    # turn off gradient-computation
+    with torch.no_grad():
         for data, target in test_loader:
-            indx_target = data.clone()
+            indx_target = target.clone()
             data = data.to(train_config.device)
 
             target = target.to(train_config.device)
+
             output = model(data)
-
+            # add loss for each mini batch
             test_loss += F.cross_entropy(output, target).item()
-            prob = F.softmax(test_loss, dim=1)
 
+            # get probability score using softmax
+            prob = F.softmax(output, dim=1)
+
+            # get the index of the max probability
             pred = prob.data.max(dim=1)[1]
 
-            count_correct_prediction = pred.cpu().eq(indx_target).sum()
+            # add correct prediction count
+            count_corect_predictions += pred.cpu().eq(indx_target).sum()
 
+        # average over number of mini-batches
         test_loss = test_loss / len(test_loader)
 
-        accuracy = 100* count_correct_prediction/len(test_loader.dataset)
-        return  test_loss, accuracy/100.0
+        # average over number of dataset
+        accuracy = 100. * count_corect_predictions / len(test_loader.dataset)
 
-
-
-
-
-
+        print(
+            '\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+                test_loss, count_corect_predictions, len(test_loader.dataset), accuracy
+            )
+        )
+    return test_loss, accuracy / 100.0
 
 
 
@@ -216,6 +288,100 @@ def val(train_config : TrainingConfiguration,
 ################################################################
 ################ Step 6 - Main   ######################
 ################################################################
+def main(system_configuration=SystemConfiguration(), training_configuration=TrainingConfiguration()):
+    # system configuration
+    setup_system(system_configuration)
+
+    # batch size
+    batch_size_to_set = training_configuration.batch_size
+    # num_workers
+    num_workers_to_set = training_configuration.num_workers
+    # epochs
+    epoch_num_to_set = training_configuration.epochs_count
+
+    # if GPU is available use training config,
+    # else lower batch_size, num_workers and epochs count
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+        batch_size_to_set = 16
+        num_workers_to_set = 2
+        epoch_num_to_set = 5
+
+    # data loader
+    train_loader, test_loader = get_data(
+        batch_size=batch_size_to_set,
+        data_root=training_configuration.data_root,
+        num_workers=num_workers_to_set
+    )
+
+    # Update training configuration
+    training_configuration = TrainingConfiguration(
+        device=device,
+        epochs_count=epoch_num_to_set,
+        batch_size=batch_size_to_set,
+        num_workers=num_workers_to_set
+    )
+
+    # initiate model
+    model = LeNet5()
+
+    # send model to device (GPU/CPU)
+    model.to(training_configuration.device)
+
+    # optimizer
+    optimizer = optim.SGD(
+        model.parameters(),
+        lr=training_configuration.learning_rate
+    )
+
+    best_loss = torch.tensor(np.inf)
+
+    # epoch train/test loss
+    epoch_train_loss = np.array([])
+    epoch_test_loss = np.array([])
+
+    # epoch train/test accuracy
+    epoch_train_acc = np.array([])
+    epoch_test_acc = np.array([])
+
+    # training time measurement
+    t_begin = time.time()
+    for epoch in range(training_configuration.epochs_count):
+
+        train_loss, train_acc = train(training_configuration, model, optimizer, train_loader, epoch)
+
+        epoch_train_loss = np.append(epoch_train_loss, [train_loss])
+
+        epoch_train_acc = np.append(epoch_train_acc, [train_acc])
+
+        elapsed_time = time.time() - t_begin
+        speed_epoch = elapsed_time / (epoch + 1)
+        speed_batch = speed_epoch / len(train_loader)
+        eta = speed_epoch * training_configuration.epochs_count - elapsed_time
+
+        print(
+            "Elapsed {:.2f}s, {:.2f} s/epoch, {:.2f} s/batch, ets {:.2f}s".format(
+                elapsed_time, speed_epoch, speed_batch, eta
+            )
+        )
+
+        if epoch % training_configuration.test_interval == 0:
+            current_loss, current_accuracy = validate(training_configuration, model, test_loader)
+
+            epoch_test_loss = np.append(epoch_test_loss, [current_loss])
+
+            epoch_test_acc = np.append(epoch_test_acc, [current_accuracy])
+
+            if current_loss < best_loss:
+                best_loss = current_loss
+
+    print("Total time: {:.2f}, Best Loss: {:.3f}".format(time.time() - t_begin, best_loss))
+
+    return model, epoch_train_loss, epoch_train_acc, epoch_test_loss, epoch_test_acc
+
+model, epoch_train_loss, epoch_train_acc, epoch_test_loss, epoch_test_acc = main()
 
 
 
@@ -224,7 +390,36 @@ def val(train_config : TrainingConfiguration,
 ################################################################
 ################ Step 7 - Plot Accuracy   ######################
 ################################################################
+# Plot loss
+import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = (10, 6)
+x = range(len(epoch_train_loss))
 
+
+plt.figure
+plt.plot(x, epoch_train_loss, color='r', label="train loss")
+plt.plot(x, epoch_test_loss, color='b', label="validation loss")
+plt.xlabel('epoch no.')
+plt.ylabel('loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+
+
+# Plot loss
+plt.rcParams["figure.figsize"] = (10, 6)
+x = range(len(epoch_train_loss))
+
+
+plt.figure
+plt.plot(x, epoch_train_acc, color='r', label="train accuracy")
+plt.plot(x, epoch_test_acc, color='b', label="validation accuracy")
+plt.xlabel('epoch no.')
+plt.ylabel('accuracy')
+plt.legend(loc='center right')
+plt.title('Training and Validation Accuracy')
+plt.show()
 
 
 
