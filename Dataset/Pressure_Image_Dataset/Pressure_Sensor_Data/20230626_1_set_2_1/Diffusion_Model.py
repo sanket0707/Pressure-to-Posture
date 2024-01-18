f347cc98d5772d5082e3960aecc3dd8254ac05f1
sanket kachole <sanketkachole0707@gmail.com>
2024-01-18 08:54:47 +0000

    Dataset extracted from Pickle file in form of Pressure Sensor Images

--- Dataset/Pressure_Image_Dataset/Pressure_Sensor_Data/20230626_1_set_2_1/Diffusion_Model.py
+++ Dataset/Pressure_Image_Dataset/Pressure_Sensor_Data/20230626_1_set_2_1/Diffusion_Model.py
@@ -10,7 +10,7 @@ import os
 import numpy as np
 import pandas as pd
 from PIL import Image
-
+import torchvision
 
 class PressureSensorDataset(Dataset):
     """
@@ -103,39 +103,71 @@ class PressureSensorDataset(Dataset):
 
 
 
+def main():
+
+    data_root = r'C:\Users\sanke\PycharmProjects\Pressure-to-Posture\Dataset\Pressure_Image_Dataset\Pressure_Sensor_Data'
+
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor()
+        ])
+
+    train_dataset =  PressureSensorDataset(data_root, train=True, image_shape=None, transform=preprocess)
+    test_dataset = PressureSensorDataset(data_root, train=False, image_shape=None, transform=preprocess)
+    '''
+    print('Length of the dataset: {}'.format(len(train_dataset)))
+    img, trgt = train_dataset[500]
+    print('Label: {}'.format(trgt))
+    print(img)
+    #img.show()
+    
+    
+    from PIL import Image, ImageTk
+    import tkinter as tk
+    
+    
+    # Create a Tkinter window
+    window = tk.Tk()
+    window.title("Pressure Sensor Image")
+    # Convert the PIL image to a format Tkinter can use
+    tk_img = ImageTk.PhotoImage(img)
+    # Create a Label widget to display your image
+    label = tk.Label(window, image=tk_img)
+    label.pack()
+    # Run the application
+    window.mainloop()
+    
+    '''
+    # dataloader with dataset
+    train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=15,shuffle=True,num_workers=2)
+
+    # dataloader with dataset
+    test_loader = torch.utils.data.DataLoader(test_dataset,batch_size=15,shuffle=True,num_workers=2)
+
+    # Plot few images
+    plt.rcParams["figure.figsize"] = (15, 9)
+    plt.figure
+    for images, labels in test_loader:
+        for i in range(len(labels)):
+            plt.subplot(3, 5, i + 1)
+            img = F.to_pil_image(images[i])
+            plt.imshow(img)
+            plt.gca().set_title('Target: {0}'.format(labels[i]))
+        plt.show()
+        break
 
 
 
 
-data_root = r'C:\Users\sanke\PycharmProjects\Pressure-to-Posture\Dataset\Pressure_Image_Dataset\Pressure_Sensor_Data'
-
-train_dataset =  PressureSensorDataset(data_root, train=True, image_shape=256)
-
-print('Length of the dataset: {}'.format(len(train_dataset)))
-
-img, trgt = train_dataset[500]
-
-print('Label: {}'.format(trgt))
-print(img)
-img.show()
-
-
-
-
-import numpy as np
-# Convert the PIL Image to a NumPy array
-img_array = np.array(img)
-# Print the NumPy array
-#print(img_array)
-
-
-# Assuming img_array is an RGB image array
-max_values = np.max(img_array, axis=(0, 1))  # Max in each channel
-min_values = np.min(img_array, axis=(0, 1))  # Min in each channel
-
-
-
+if __name__ == '__main__':
+    main()
 ###################################################################
 ############### Create Custom Dataset - Ends ##################
 ###################################################################
+
+
+
+
+
 
